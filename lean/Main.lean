65af51d01f15c07e5f11eada39d07454e29d733b
samuelburnham <45365069+samuelburnham@users.noreply.github.com>
2025-09-26 10:17:58 -0400

    Add Lean template

---
+++ lean/Main.lean
@@ -0,0 +1,4 @@
+import Template
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lean/flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Lean Nix flake";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
+
+  inputs = {
+    # System packages
+    nixpkgs.follows = "lean4-nix/nixpkgs";
+
+    # Lean 4 & Lake
+    lean4-nix.url = "github:lenianiva/lean4-nix";
+
+    # Helper: flake-parts for easier outputs
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    lean4-nix,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      # Systems we want to build for
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {
+        system,
+        pkgs,
+        ...
+      }: {
+        # Lean overlay
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(lean4-nix.readToolchainFile ./lean-toolchain)];
+        };
+
+        packages.default =
+          ((lean4-nix.lake {inherit pkgs;}).mkPackage {
+            src = ./.;
+            roots = ["Main" "Template"];
+          })
+          .executable;
+
+        # Provide a unified dev shell with Lean + Rust
+        devShells.default = pkgs.mkShell {
+          LEAN_SYSROOT = "${pkgs.lean.lean-all}";
+          packages = with pkgs; [
+            lean.lean # Lean compiler
+            lean.lean-all # Includes Lake, stdlib, etc.
+          ];
+        };
+      };
+    };
+}
