6512802867df8223d6bca72ba4ee2bd71708ffc8
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2026-05-12 21:02:45 -0400

    Update versions and add top-level CI (#1)

--- lean/flake.nix
+++ lean/flake.nix
@@ -1,33 +1,35 @@
 {
   description = "Lean Nix flake";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.garnix.io"
-    ];
-    extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-  };
+  # First enable Garnix GH App for the repo
+  # nixConfig = {
+  #   extra-substituters = [
+  #     "https://cache.garnix.io"
+  #   ];
+  #   extra-trusted-public-keys = [
+  #     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+  #   ];
+  # };
 
   inputs = {
     # System packages
     nixpkgs.follows = "lean4-nix/nixpkgs";
 
     # Lean 4 & Lake
-    lean4-nix.url = "github:argumentcomputer/lean4-nix?ref=7653ecd6d80710f42fcc2c117144698b18acdfc1";
+    lean4-nix.url = "github:lenianiva/lean4-nix";
 
     # Helper: flake-parts for easier outputs
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs @ {
-    nixpkgs,
-    flake-parts,
-    lean4-nix,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      lean4-nix,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       # Systems we want to build for
       systems = [
         "aarch64-darwin"
@@ -36,50 +38,55 @@
         "x86_64-linux"
       ];
 
-      perSystem = {
-        system,
-        pkgs,
-        ...
-      }: let
-        lake2nix = pkgs.callPackage lean4-nix.lake {};
-        lakeArgs = {
-          src = ./.;
-        };
-        lakeDeps = lake2nix.buildDeps lakeArgs;
-        lakeBuildArgs =
-          lakeArgs
-          // {
+      perSystem =
+        {
+          system,
+          pkgs,
+          ...
+        }:
+        let
+          lake2nix = pkgs.callPackage lean4-nix.lake { };
+          lakeArgs = {
+            src = ./.;
+          };
+          lakeDeps = lake2nix.buildDeps lakeArgs;
+          lakeBuildArgs = lakeArgs // {
             inherit lakeDeps;
           };
-        leanLib = lake2nix.mkPackage (lakeBuildArgs
-          // {
-            name = "Template";
-            buildLibrary = true;
-          });
-        leanBin = lake2nix.mkPackage (lakeBuildArgs
-          // {
-            lakeArtifacts = leanLib;
-            installArtifacts = false;
-            name = "template";
-          });
-      in {
-        # Lean overlay
-        _module.args.pkgs = import nixpkgs {
-          inherit system;
-          overlays = [(lean4-nix.readToolchainFile ./lean-toolchain)];
-        };
+          leanLib = lake2nix.mkPackage (
+            lakeBuildArgs
+            // {
+              name = "Template";
+              buildLibrary = true;
+            }
+          );
+          leanBin = lake2nix.mkPackage (
+            lakeBuildArgs
+            // {
+              lakeArtifacts = leanLib;
+              installArtifacts = false;
+              name = "template";
+            }
+          );
+        in
+        {
+          # Lean overlay
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (lean4-nix.readToolchainFile ./lean-toolchain) ];
+          };
 
-        packages = {
-          default = leanLib;
-          lean-bin = leanBin;
-        };
+          packages = {
+            default = leanLib;
+            lean-bin = leanBin;
+          };
 
-        # Provide a unified dev shell with Lean + Rust
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            lean.lean-all # Includes Lean compiler, lake, stdlib, etc.
-          ];
+          # Provide a unified dev shell with Lean + Rust
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              lean.lean-all # Includes Lean compiler, lake, stdlib, etc.
+            ];
+          };
         };
-      };
     };
 }
